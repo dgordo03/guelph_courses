455be0bfefa8c305dabd77fd3dcefa84e6d372e2
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-11-22 22:56:11 -0500

    dictionary created which stores all course types

--- src/scraper.py
+++ src/scraper.py
@@ -4,12 +4,14 @@ from BeautifulSoup import BeautifulSoup
 url='https://www.uoguelph.ca/registrar/calendars/undergraduate/2017-2018/c12/index.shtml'
 response = requests.get(url)
 html = response.content
-
 soup = BeautifulSoup(html)
-
 table = soup.find('div', attrs={'id':'sidebar'})
+
+course_descr = {}
 
 for unorderd_list in table.findAll('ul'):
     for list_item in unorderd_list.findAll('a'):
-        print list_item['href']
-        print list_item.text.replace('&nbsp;', '')
+        href = list_item['href']
+        name = list_item.text.replace('&nbsp', '')
+        course_descr[name] = href
+
