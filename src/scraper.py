0602a47c1e4d1b634e5548df7e56d6244a6f12f4
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-12-13 20:03:23 +0000

    Cleaned up syntax

--- src/scraper.py
+++ src/scraper.py
@@ -1,21 +1,20 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-base_url = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/2017-2018/c12/'
+base_url = "https://www.uoguelph.ca/registrar/calendars/undergraduate/\
+2017-2018/c12/"
 exam_base = "https://www.uoguelph.ca/registrar/scheduling/"
- 
+
+
 def buildingCodes():
     url = exam_base + "buildingcodes-col"
-    
     response = requests.get(url)
     html = response.content
     soup = BeautifulSoup(html)
-
-    mainDiv = soup.find('div', attrs={'id':'main'})
-    containerDiv = mainDiv.find('div', attrs={'class':'container'})
-    contentDiv = containerDiv.find('div', attrs={'id':'content'})
+    mainDiv = soup.find('div', attrs={'id': 'main'})
+    containerDiv = mainDiv.find('div', attrs={'class': 'container'})
+    contentDiv = containerDiv.find('div', attrs={'id': 'content'})
     buildings = {}
-
     for code in contentDiv.findAll('p'):
         full_name = code.text.replace('&nbsp', '')
         full_name = full_name.encode('utf8')
@@ -27,24 +26,20 @@ def buildingCodes():
             if name != '':
                 build_name += name
         buildings[build_code] = build_name
-
     return buildings
+
 
 def examInformation():
     url = exam_base + "exam_fall"
-
     response = requests.get(url)
     html = response.content
     soup = BeautifulSoup(html)
-
-    mainDiv = soup.find('div', attrs={'id':'main'})
-    containerDiv = mainDiv.find('div', attrs={'class':'container'})
-    contentDiv = containerDiv.find('div', attrs={'id':'content'})
-    table = contentDiv.find('table', attrs={'class':'exams'})
+    mainDiv = soup.find('div', attrs={'id': 'main'})
+    containerDiv = mainDiv.find('div', attrs={'class': 'container'})
+    contentDiv = containerDiv.find('div', attrs={'id': 'content'})
+    table = contentDiv.find('table', attrs={'class': 'exams'})
     body = table.find('tbody')
-    
     exams = []
-
     for row in body.findAll('tr'):
         curr_exam = row.findAll('td')
         exam_info = {}
@@ -53,44 +48,36 @@ def examInformation():
         exam_info['end'] = curr_exam[4].text.replace('&nbsp', '')
         exam_info['location'] = curr_exam[6].text.replace('&nbsp', '')
         exams.append(exam_info)
-    
     return exams
 
-examInformation()
 
 def scrapeCourse(page):
     url = base_url + page
-    
     response = requests.get(url)
     html = response.content
     soup = BeautifulSoup(html)
-    
-    mainDiv = soup.find('div', attrs={'id':'main'})
-    contentDiv = mainDiv.find('div', attrs={'id':'content'})
-   
+    mainDiv = soup.find('div', attrs={'id': 'main'})
+    contentDiv = mainDiv.find('div', attrs={'id': 'content'})
     all_courses = {}
- 
-    for courses in contentDiv.findAll('div', attrs={'class':'course'}):
+    for courses in contentDiv.findAll('div', attrs={'class': 'course'}):
         curr_course = {}
         prereq_course = {}
         restr_course = {}
-
         table = courses.find('table')
-        title_class = table.find('tr', attrs={'class':'title'})
-        title =  title_class.find('a').text
-        
-        descr_class = table.find('tr', attrs={'class':'description'})
+        title_class = table.find('tr', attrs={'class': 'title'})
+        title = title_class.find('a').text
+        descr_class = table.find('tr', attrs={'class': 'description'})
         description = descr_class.find('td').text
         curr_course['description'] = description
-        restr_class = table.find('tr', attrs={'class':'restrictions'})
-        if restr_class != None:
+        restr_class = table.find('tr', attrs={'class': 'restrictions'})
+        if restr_class is not None:
             for restr in restr_class.findAll('a'):
                 restriction = restr.text
                 href = restr['href']
                 restr_course[restriction] = href
         curr_course['restrictions'] = restr_course
         prereq_class = table.find('tr', attrs={'class': 'prereqs'})
-        if prereq_class != None:
+        if prereq_class is not None:
             for prereq in prereq_class.findAll('a'):
                 requisite = prereq.text
                 href = prereq['href']
@@ -99,22 +86,17 @@ def scrapeCourse(page):
         all_courses[title] = curr_course
     return all_courses
 
+
 def scrapeFaculty():
     url = base_url + 'index.shtml'
-
     response = requests.get(url)
     html = response.content
     soup = BeautifulSoup(html)
-    div = soup.find('div', attrs={'id':'sidebar'})
-
+    div = soup.find('div', attrs={'id': 'sidebar'})
     faculty = {}
-
     for unorderd_list in div.findAll('ul'):
         for list_item in unorderd_list.findAll('a'):
             href = list_item['href']
             name = list_item.text.replace('&nbsp', '')
             faculty[name] = href
     return faculty
-
-
-buildingCodes()
