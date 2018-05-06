9b01a730a9eefdf3809801f6fc84b6e822d9dc94
dgordo03 <daniel.gordon.0427@gmail.com>
2018-05-06 08:10:47 -0400

    header fixed for small screen sizes

--- web/php/header.php
+++ web/php/header.php
@@ -1,11 +1,17 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <ul class="nav navbar-nav">
+      <!-- <ul class="nav navbar-nav">
         <li><a class="glyphicon glyphicon-home" href="../../index.php"></a></li>
-      </ul>
+      </ul> -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#header">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="../../index.php">Home</a>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="header">
       <ul class="nav navbar-nav">
         <li><a href="./exams.php">Exam Schedule</a></li>
         <li><a href="./schedule.php">Class Schedule</a></li>
