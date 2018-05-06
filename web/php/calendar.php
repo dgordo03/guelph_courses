69a6ba099b4bf1059cae210bc48705317fd6ed32
dgordo03 <daniel.gordon.0427@gmail.com>
2018-05-05 22:57:29 -0400

    calendar and course table updated

--- web/php/calendar.php
+++ web/php/calendar.php
@@ -1,29 +1,31 @@
 <link rel="stylesheet" type="text/css" href="../css/calendar.css">
-<div class="row">
-  <div class="col-xs-1"></div>
-  <div class="col-xs-2 border-bottom">
-    <span>Monday</span>
+<div class="calendar">
+  <div class="row">
+    <div class="col-xs-1"></div>
+    <div class="col-xs-2 border-bottom">
+      <span>Monday</span>
+    </div>
+    <div class="col-xs-2 border-bottom">
+      <span>Tuesday</span>
+    </div>
+    <div class="col-xs-2 border-bottom">
+      <span>Wednesday</span>
+    </div>
+    <div class="col-xs-2 border-bottom">
+      <span>Thursday</span>
+    </div>
+    <div class="col-xs-2 border-bottom">
+      <span>Friday</span>
+    </div>
   </div>
-  <div class="col-xs-2 border-bottom">
-    <span>Tuesday</span>
-  </div>
-  <div class="col-xs-2 border-bottom">
-    <span>Wednesday</span>
-  </div>
-  <div class="col-xs-2 border-bottom">
-    <span>Thursday</span>
-  </div>
-  <div class="col-xs-2 border-bottom">
-    <span>Friday</span>
-  </div>
+  <?php
+  for ($time = 8.5; $time < 22.5; $time += 0.5) {
+    print "<div class=\"row\"><div class=\"col-xs-1 border-right\"><span>$time</span></div>";
+    print "<div class=\"col-xs-2 mon rectangle\"></div>";
+    print "<div class=\"col-xs-2 tues rectangle\"><span></span></div>";
+    print "<div class=\"col-xs-2 wed rectangle\"><span></span></div>";
+    print "<div class=\"col-xs-2 thurs rectangle\"><span></span></div>";
+    print "<div class=\"col-xs-2 fri rectangle\"><span></span></div></div>";
+  }
+  ?>
 </div>
-<?php
-for ($time = 8.5; $time < 22.5; $time += 0.5) {
-  print "<div class=\"row\"><div class=\"col-xs-1 border-right\"><span>$time</span></div>";
-  print "<div class=\"col-xs-2 mon rectangle\"></div>";
-  print "<div class=\"col-xs-2 tues rectangle\"><span></span></div>";
-  print "<div class=\"col-xs-2 wed rectangle\"><span></span></div>";
-  print "<div class=\"col-xs-2 thurs rectangle\"><span></span></div>";
-  print "<div class=\"col-xs-2 fri rectangle\"><span></span></div></div>";
-}
-?>
--- web/php/schedule.php
+++ web/php/schedule.php
@@ -110,93 +110,15 @@
         <button id="clearAllCourses" type="button" class="btn btn-danger col-xs-12">Clear All Courses</button>
       </div>
     </div>
-    <div class="col-xs-12 col-md-6 course_information">
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-      if (!empty($_GET['course_term'])) {
-        $args['term'] = $_GET['course_term'];
-        if (!empty($_GET['course_number'])) {
-          $args['number'] = $_GET['course_number'];
-        }
-        if (!empty($_GET['course_subject'])) {
-          $args['subject'] = $_GET['course_subject'];
-        }
-        if (!empty($_GET['course_level'])) {
-          $args['level'] = $_GET['course_level'];
-        }
-        if (sizeof($args) > 3) {
-          $pyscript = 'C:\\xampp\\htdocs\\GitHub\\guelph_courses\\src\\getCourse.py';
-          $python = 'C:\\Python27\\python.exe';
-          $args_str = "";
-          foreach ($args as $key => $value) {
-              $args_str .= " {$key}={$value}";
-          }
-          $cmd = "$python $pyscript $args_str";
-          exec($cmd, $output, $ret);
-
-          if (sizeof($output) > 0) {
-            print "<div class=\"list-group\">";
-            print "<a href=\"#\" class=\"list-group-item col-xs-12\">";
-            print "<h5 class=\"list-group-item-heading col-xs-3\">Course</h5>";
-            print "<h5 class=\"list-group-item-heading col-xs-3\">Times</h5>";
-            print "<h5 class=\"list-group-item-heading col-xs-3\">Faculty</h5>";
-            print "<h5 class=\"list-group-item-heading col-xs-3\">Capacity</h5>";
-            print "</a>";
-          } else {
-            print "<div class=\"list-group\">";
-            print "<a href=\"#\" class=\"list-group-item col-xs-12\">";
-            print "<h5 class=\"list-group-item-heading col-xs-3\">No Available Sections</h5>";
-            print "</a>";
-          }
-
-          foreach ($output as $iter) {
-            // split up the information from one another
-            $temp = explode("meeting=", $iter);
-            $temp = explode("faculty=", $temp[1]);
-            $meeting = $temp[0];
-            $temp = explode("capacity=", $temp[1]);
-            $faculty = $temp[0];
-            $temp = explode("link=", $temp[1]);
-            $capacity = $temp[0];
-            $link = $temp[1];
-            // split up the info from entries
-            $meeting = explode("NEXT", $meeting);
-            $faculty = explode("NEXT", $faculty);
-            $capacity = explode("NEXT", $capacity);
-            $link = explode("NEXT", $link);
-
-            print "<a href=\"#\" class=\"list-group-item col-xs-12 section\">";
-            $link_t = "";
-            foreach ($link as $value) {
-              $link_t .= $value;
-            }
-            print "<p class=\"list-group-item-heading col-xs-3 course\">$link_t</p>";
-            $meeting_t = "";
-            foreach ($meeting as $value) {
-              $meeting_t .= $value;
-            }
-            print "<p class=\"list-group-item-heading col-xs-3 times\">$meeting_t</p>";
-            $faculty_t = "";
-            foreach ($faculty as $value) {
-              $faculty_t .= $value;
-            }
-            print "<p class=\"list-group-item-heading col-xs-3 faculty\">$faculty_t</p>";
-            $capacity_t = "";
-            foreach ($capacity as $value) {
-              $capacity_t .= $value;
-            }
-            print "<p class=\"list-group-item-heading col-xs-3 capacity\">$capacity_t</p>";
-            print "</a>";
-          }
-        }
-      }
-      }
-    ?>
+    <div class="col-xs-12">
+      <div class="col-xs-12 col-md-6">
+        <?php include("courseTable.php") ?>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <?php include("calendar.php") ?>
+      </div>
     </div>
-  </div>
-  <div class="col-md-6 calendar">
-    <?php include("calendar.php"); ?>
-  </div>
+
 
   <!-- Modal -->
   <div class="modal fade" id="waitModal" tabindex="-1" role="dialog" aria-labelledby="waitModalLabel" data-backdrop="static" data-keyboard="false">
